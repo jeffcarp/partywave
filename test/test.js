92179542373bc8e78fc0801049fbd0b0629269fb
Jeff Carpenter <gcarpenterv@gmail.com>
2014-07-10 23:55:31 +0000

    Add first test, yay

--- test/test.js
+++ test/test.js
@@ -1,3 +1,30 @@
-console.log('aww yiss');
+var app = require('../app');
+var expect = require('expect.js');
+var request = require('superagent');
 
-window.test = 'test';
+var port = 3005;
+var server;
+
+var url = function(path) {
+  return 'http://localhost:' + port + path;
+};
+
+before(function(done) {
+  server = app.listen(port, done);
+});
+
+after(function(done) {
+  server.close(done);
+});
+
+describe('GET /', function() {
+  it('does stuff', function(done) {
+    request.get(url('/')).end(function(res) {
+
+      expect(res.status).to.equal(200);
+      expect(res.text).to.contain('ConcatJS');
+
+      done();
+    });
+  });
+});
