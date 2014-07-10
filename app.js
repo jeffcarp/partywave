c0d7f359b59207c29570ab0425e5dc7c7f0e716d
Jeff Carpenter <gcarpenterv@gmail.com>
2014-07-10 22:11:36 +0000

    Refactor a bit and get browserify writing to disk

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var fs = require('fs');
+var browserify = require('browserify');
+var b = browserify();
 var app = module.exports = express();
 
 app.set('view engine', 'ejs');
@@ -9,7 +11,54 @@ app.get('/', function (req, res) {
 });
 
 app.get('*', function (req, res) {
+
+  //var query = 'react+angular';
+  var query = req.path.slice(1);
+
+  var libraries = query.split('+').sort();
+
   res.header("Content-Type", "application/javascript");
-  var text = fs.readFileSync('./test.js');
-  res.send(text);
+  var filename = './builds/'+libraries.join('+');
+
+  if (fs.existsSync(filename)) {
+    sendFile(req, res, filename);
+    console.log(filename, 'existing file found, sending');
+    return;
+  }
+
+  var notFound = [];
+  libraries.forEach(function(libName) {
+    try {
+      require.resolve(libName);
+    } catch(e) {
+      notFound.push(libName);
+    }
+  });
+
+  if (notFound.length) {
+    var msg = 'Unable to find ' + notFound.join(', ');
+    console.log(filename, msg);
+    res.send(msg);
+    return;
+  }
+
+  libraries.forEach(function(libName) {
+    b.add(libName);
+  });
+
+  console.log('about to create ', filename);
+  var file = fs.createWriteStream(filename);
+  var writePipe = b.bundle().pipe(file).on('end', function() {
+    console.log('ended yes');
+  });
+
+  if (fs.existsSync(filename)) {
+    sendFile(req, res, filename);
+    return;
+  }
 });
+
+var sendFile = function(req, res, filename) {
+  var fileContents = fs.readFileSync(filename, 'utf8');
+  res.send(fileContents);
+};
