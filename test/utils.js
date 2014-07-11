8085239611318ebd520c8e298db0b9f721e4ac05
Jeff Carpenter <gcarpenterv@gmail.com>
2014-07-11 20:37:17 +0000

    Add very basic bundle test

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,6 @@
 var utils = require('../utils');
 var expect = require('expect.js');
+var browserify = require('browserify');
 
 describe('utils', function() {
 
@@ -50,7 +51,30 @@ describe('utils', function() {
   });
 
   describe('bundle', function() {
-    it('is pretty cool');
+    it('successfully bundles a few packages', function(done) {
+
+      var libraries = [
+        'array-map',
+        'array-reduce'
+      ];
+
+      var b = browserify();
+      libraries.forEach(function(libName) {
+        b.require(libName);
+      });
+
+      utils.bundle(libraries, function(utilsBundledStr) {
+
+        b.bundle({}, function(err, bundledStr) {
+          if (err) console.error(err);
+
+          expect(utilsBundledStr).to.equal(bundledStr);
+          done();
+        });
+
+      });
+
+    });
   });
 
 });
