a9ce9613affffafb588e05f3a78c63eaf099e4e1
Jeff Carpenter <gcarpenterv@gmail.com>
2014-07-11 21:49:11 +0000

    Re-implement caching

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var u = require('./utils');
+var fs = require('fs');
 var app = module.exports = express();
 
 app.get('/', function (req, res) {
@@ -11,6 +12,13 @@ app.get('*', function (req, res) {
 
   var query = req.path.slice(1);
   var libraries = query.split('+').sort();
+  var filename = './builds/'+libraries.join('+');
+
+  if (fs.existsSync(filename)) {
+    var fileContents = fs.readFileSync(filename, 'utf8');
+    res.send(fileContents);
+    return;
+  }
 
   var unavailable = libraries.filter(function(lib) {
     return !u.moduleInstalled(lib);
@@ -26,63 +34,6 @@ app.get('*', function (req, res) {
 
   u.bundle(libraries, function(bundledStr) {
     res.send(bundledStr);
+    fs.writeFileSync(filename, bundledStr);
   });
 });
-
-/*
-app.get('*', function (req, res) {
-
-  //var query = 'react+angular';
-  var query = req.path.slice(1);
-
-  var libraries = query.split('+').sort();
-
-  res.header("Content-Type", "application/javascript");
-  var filename = './builds/'+libraries.join('+');
-
-  if (fs.existsSync(filename)) {
-    sendFile(req, res, filename);
-    console.log(filename, 'existing file found, sending');
-    return;
-  }
-
-  var notFound = [];
-  libraries.forEach(function(libName) {
-    try {
-      require.resolve(libName);
-    } catch(e) {
-      notFound.push(libName);
-    }
-  });
-
-  if (notFound.length) {
-    var msg = 'Unable to find ' + notFound.join(', ');
-    console.log(filename, msg);
-    res.send(msg);
-    return;
-  }
-
-  libraries.forEach(function(libName) {
-    b.require(libName);
-  });
-
-  console.log('about to create ', filename);
-  var file = fs.createWriteStream(filename);
-  var writePipe = b.bundle().pipe(file).on('end', function() {
-    console.log('ended yes');
-  });
-
-  if (fs.existsSync(filename)) {
-    sendFile(req, res, filename);
-    return;
-  }
-
-  // TODO: Use new way of stringifying browserify output
-  // TODO: Write cache file after you send the response
-});
-
-var sendFile = function(req, res, filename) {
-  var fileContents = fs.readFileSync(filename, 'utf8');
-  res.send(fileContents);
-};
-*/
